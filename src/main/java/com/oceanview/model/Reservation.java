4da3cab5b274eebd8e4b4be0d0ae6bd783b5a348
Asterix7339 <sathamkaleel@gmail.com>
2026-03-02 04:09:07 +0530

    create the Reservation Model

---
+++ src/main/java/com/oceanview/model/Reservation.java
@@ -0,0 +1,93 @@
+package com.oceanview.model;
+
+import java.sql.Date;
+
+public class Reservation {
+
+    private int id;
+    private String reservationNumber;
+
+    private String guestName;
+    private String address;
+    private String contactNumber;
+
+    private int roomTypeId;          // FK to room_types.id
+    private Date checkIn;
+    private Date checkOut;
+
+    private double totalAmount;      // can be 0 if not calculated yet
+
+    public Reservation() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getReservationNumber() {
+        return reservationNumber;
+    }
+
+    public void setReservationNumber(String reservationNumber) {
+        this.reservationNumber = reservationNumber;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public int getRoomTypeId() {
+        return roomTypeId;
+    }
+
+    public void setRoomTypeId(int roomTypeId) {
+        this.roomTypeId = roomTypeId;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
