4b77d19e01a3df1dd2bc525de4a687beeff2238a
Asterix7339 <sathamkaleel@gmail.com>
2026-03-04 03:40:22 +0530

    Admin able to update rooms type and count

---
+++ src/main/java/com/oceanview/model/RoomSummary.java
@@ -0,0 +1,53 @@
+package com.oceanview.model;
+
+public class RoomSummary {
+
+    private int roomTypeId;
+    private String roomTypeCode; // STANDARD / DELUXE / SUITE
+
+    private int totalRooms;
+    private int occupiedRooms;
+    private int availableRooms;
+
+    public RoomSummary() {}
+
+    public int getRoomTypeId() {
+        return roomTypeId;
+    }
+
+    public void setRoomTypeId(int roomTypeId) {
+        this.roomTypeId = roomTypeId;
+    }
+
+    public String getRoomTypeCode() {
+        return roomTypeCode;
+    }
+
+    public void setRoomTypeCode(String roomTypeCode) {
+        this.roomTypeCode = roomTypeCode;
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public void setTotalRooms(int totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+
+    public int getOccupiedRooms() {
+        return occupiedRooms;
+    }
+
+    public void setOccupiedRooms(int occupiedRooms) {
+        this.occupiedRooms = occupiedRooms;
+    }
+
+    public int getAvailableRooms() {
+        return availableRooms;
+    }
+
+    public void setAvailableRooms(int availableRooms) {
+        this.availableRooms = availableRooms;
+    }
+}
